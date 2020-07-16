338ce210d21fc0e437cd2542cd04033782b7cc23
Vladmir <roldugin.67@gmail.com>
2020-07-16 18:54:04 +0300

    Добавьте файлы проекта.

---
+++ Navigation_Page/Navigation_Page/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Navigation_Page
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Page1 pg1 = new Page1();
+
+            NavigationPage.SetHasNavigationBar(pg1, false);
+
+            //NavigationPage.SetHasBackButton(pg1, false); - убрать кнопку назад
+
+            await Navigation.PushAsync(pg1);
+        }
+    }
+}
